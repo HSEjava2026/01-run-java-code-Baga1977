53083bdbd03c4e3c7d2973c4bda109cd9bdbf006
Baga1977 <bogdansport14@gmail.com>
2026-02-08 18:23:10 +0300

    Выполнены уровни 1-4

---
+++ src/ru/hse/lab1/Hello.java
@@ -0,0 +1,13 @@
+package ru.hse.lab1;
+
+/**
+ * Класс для приветствия.
+ * @author Соколов Б.А.
+ * @version 1.0
+ * @since 2026
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
